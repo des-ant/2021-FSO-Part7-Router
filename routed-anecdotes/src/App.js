b910d7134a99ee243912845a630250c7bbbaea65
des-ant <57375593+des-ant@users.noreply.github.com>
2021-11-10 22:05:38 +1100

    Simplifed the anecdote creation form of your application with the useField custom hook. -ex7.4

--- routed-anecdotes/src/App.js
+++ routed-anecdotes/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useField } from './hooks';
 
 import {
   Switch,
@@ -67,18 +68,18 @@ const Footer = () => (
 );
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('');
-  const [author, setAuthor] = useState('');
-  const [info, setInfo] = useState('');
-
   const history = useHistory();
+  const content = useField('text', 'content');
+  const author = useField('text', 'author');
+  const info = useField('text', 'info');
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0
     });
     history.push('/');
@@ -90,15 +91,15 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input {...content} />
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input {...author} />
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+          <input {...info} />
         </div>
         <button>create</button>
       </form>
