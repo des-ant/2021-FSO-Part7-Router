eede0a4f29346a8dd8743c6ccae791ac1a68889f
des-ant <57375593+des-ant@users.noreply.github.com>
2021-11-16 15:10:11 +1100

    Created the actual reducer for application by combining the existing reducers with the combineReducers function. Installed Redux DevTools to monitor state of the Redux store from console of the browser.

--- bloglist-frontend/src/index.js
+++ bloglist-frontend/src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, combineReducers } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import { Provider } from 'react-redux';
 import App from './App';
 import './index.css';
+
 import notificationReducer from './reducers/notificationReducer';
 
-export const store = createStore(notificationReducer);
+const reducer = combineReducers({
+  notification: notificationReducer,
+});
+
+export const store = createStore(reducer, composeWithDevTools());
 
 ReactDOM.render(
   <Provider store={store}>
