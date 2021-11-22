7e47de399d76e5e4140bb69ef25fa014b274a8d0
des-ant <57375593+des-ant@users.noreply.github.com>
2021-11-22 13:09:58 +1100

    Updated BlogList component using MaterialUI library

--- bloglist-frontend/src/components/BlogList.js
+++ bloglist-frontend/src/components/BlogList.js
@@ -1,28 +1,37 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableRow,
+  Paper,
+} from '@material-ui/core';
 
 const BlogList = () => {
   const blogs = useSelector(state => state.blogs);
 
-  const blogStyle = {
-    paddingTop: 10,
-    paddingLeft: 2,
-    border: 'solid',
-    borderWidth: 1,
-    marginBottom: 5,
-  };
-
   return (
-    <div>
-      {blogs.map(blog =>
-        <div style={blogStyle} key={blog.id}>
-          <Link to={`/blogs/${blog.id}`}>
-            {`${blog.title} ${blog.author}`}
-          </Link>
-        </div>
-      )}
-    </div>
+    <TableContainer component={Paper}>
+      <Table>
+        <TableBody>
+          {blogs.map(blog => (
+            <TableRow key={blog.id}>
+              <TableCell>
+                <Link to={`/blogs/${blog.id}`}>
+                  {blog.title}
+                </Link>
+              </TableCell>
+              <TableCell>
+                {blog.author}
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 };
 
